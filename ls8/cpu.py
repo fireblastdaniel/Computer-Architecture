53f978e690b97b9a27835780ae4aa7d2b8fc8b15
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-07-22 14:04:04 -0600

    main project completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,38 +7,133 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.reg = [0] * 7 + [244] # empty stack pointer starts at F4
+        self.ram = [0] * 256
+        self.pc = 0
+        self.running = True
+        self.branchtable = {
+            #ALU operations
+            0b10100000: self.add,
+            0b10100001: self.sub,
+            0b10100010: self.mul,
+            0b10100011: self.div,
+            0b10100100: self.mod,
+            # 0b01100101: self.inc,
+            # 0b01100110: self.dec,
+            # 0b10100111: self.cmp,
+            # 0b10101000: self.AND,
+            # 0b01101001: self.NOT,
+            # 0b10101010: self.OR,
+            # 0b10101011: self.XOR,
+            # 0b10101100: self.SHL,
+            # 0b10101101: self.SHR
+
+            #PC mutators
+            0b01010000: self.call,
+            0b00010001: self.ret,
+
+            #Other
+            0b10000010: self.ldi,
+            0b01000111: self.prn,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+            0b00000001: self.halt
+        }
         pass
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            address = 0
+            with open(file_name) as file:
+                for line in file:
+                    split_line = line.split('#')[0]
+                    command = split_line.strip()
 
-        # For now, we've just hardcoded a program:
+                    if command == '':
+                        continue
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+                    instruction = int(command, 2)
+                    self.ram[address] = instruction
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+                    address += 1
 
+        except FileNotFoundError:
+            print(f'{sys.argv[0]}: {sys.argv[1]} file was not found')
+            sys.exit()
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+            self.pc += 3
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+            self.pc += 3
+        elif op == 'MUL':
+            self.reg[reg_a] *= self.reg[reg_b]
+            self.pc += 3
+        elif op == 'DIV':
+            self.reg[reg_a] //= self.reg[reg_b]
+            self.pc += 3
+        elif op == 'MOD':
+            self.reg[reg_a] %= self.reg[reg_b]
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
+
+    def add(self, op1, op2):
+        self.alu('ADD', op1, op2)
+
+    def sub(self, op1, op2):
+        self.alu('SUB', op1, op2)
+
+    def mul(self, op1, op2):
+        self.alu('MUL', op1, op2)
+
+    def div(self, op1, op2):
+        self.alu('DIV', op1, op2)
+
+    def mod(self, op1, op2):
+        self.alu('MOD', op1, op2)
+
+    def call(self, op1, op2):
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = self.pc + 2
+        self.pc = self.reg[op1]
+
+    def ret(self, op1, op2):
+        self.pc = self.ram[self.reg[7]]
+        self.reg[7] += 1
+
+    def ldi(self, op1, op2):
+        self.reg[op1] = op2
+        self.pc += 3
+
+    def push(self, op1, op2):
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = self.reg[op1]
+        self.pc += 2
+
+    def pop(self, op1, op2):
+        self.reg[op1] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        self.pc += 2
+
+    def prn(self, op1, op2):
+        print(self.reg[op1])
+        self.pc += 2
+
+    def halt(self, op1, op2):
+        self.running = False
 
     def trace(self):
         """
@@ -62,4 +157,9 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        while self.running:
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            self.branchtable[ir](operand_a, operand_b)
         pass
