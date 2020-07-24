8aa1dc9fbb001101126f0513a66de593b7a2f589
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-07-24 09:35:28 -0600

    finished sprint challenge

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.reg = [0] * 7 + [244] # empty stack pointer starts at F4
         self.ram = [0] * 256
         self.pc = 0
+        self.fl = 0
         self.running = True
         self.branchtable = {
             #ALU operations
@@ -20,7 +21,7 @@ class CPU:
             0b10100100: self.mod,
             0b01100101: self.inc,
             0b01100110: self.dec,
-            # 0b10100111: self.CMP,
+            0b10100111: self.CMP,
             0b10101000: self.AND,
             0b01101001: self.NOT,
             0b10101010: self.OR,
@@ -31,15 +32,27 @@ class CPU:
             #PC mutators
             0b01010000: self.call,
             0b00010001: self.ret,
+            # 0b01010010: self.int,
+            # 0b00010011: self.iret,
+            0b01010100: self.jmp,
+            0b01010101: self.jeq,
+            0b01010110: self.jne,
+            # 0b01010111: self.jgt,
+            # 0b01011000: self.jlt,
+            # 0b01011001: self.jle,
+            # 0b01011010: self.jge,
 
             #Other
             0b10000010: self.ldi,
             0b01000111: self.prn,
+            0b01001000: self.pra,
+            0b10000011: self.ld,
+            0b10000100: self.st,
             0b01000101: self.push,
             0b01000110: self.pop,
+            # 0b00000000: self.nop,
             0b00000001: self.halt
         }
-        pass
 
     def load(self, file_name):
         """Load a program into memory."""
@@ -81,7 +94,12 @@ class CPU:
         elif op == 'DEC':
             self.reg[reg_a] -= 1
         elif op == 'CMP':
-            pass
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = 0b00000100
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = 0b00000010
+            elif self.reg[reg_a] == self.reg[reg_b]:
+                self.fl = 0b00000001
         elif op == 'AND':
             self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
         elif op == 'NOT':
@@ -155,8 +173,31 @@ class CPU:
         self.pc = self.ram[self.reg[7]]
         self.reg[7] += 1
 
+    def jmp(self, op1, op2=None):
+        self.pc = self.reg[op1]
+
+    def jeq(self, op1, op2=None):
+        if self.fl == 0b00000001:
+            self.jmp(op1)
+        else:
+            self.pc += 2
+
+    def jne(self, op1, op2=None):
+        if self.fl != 0b00000001:
+            self.jmp(op1)
+        else:
+            self.pc += 2
+
     def ldi(self, op1, op2):
         self.reg[op1] = op2
+        self.pc += 3
+
+    def ld(self, op1, op2):
+        self.reg[op1] = self.ram_read(self.reg[op2])
+        self.pc += 3
+
+    def st(self, op1, op2):
+        self.ram_write(self.reg[op2], self.reg[op1])
         self.pc += 3
 
     def push(self, op1, op2=None):
@@ -171,6 +212,10 @@ class CPU:
 
     def prn(self, op1, op2=None):
         print(self.reg[op1])
+        self.pc += 2
+
+    def pra(self, op1, op2=None):
+        print(chr(self.reg[op1]))
         self.pc += 2
 
     def halt(self, op1=None, op2=None):
